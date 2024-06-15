a48c0aa208049434b0c646160d13aa408602edd9
Dan Caron <dancaron@gmail.com>
2024-06-14 20:21:40 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="agentspace",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Dan Caron",
+    author_email="dancaron@gmail.com",
+    description="A compact and easy-to-use agent framework",
+    license="MIT",
+)
